6b05611793f58221af1c256e25c8ce65dddd043f
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-12-09 09:40:37 +0000

    python check scipt added

---
+++ check_embeddings.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+import os
+import itertools
+
+from pandas.errors import EmptyDataError
+
+def main():
+
+    datasets = ["cora_ml", "citeseer", "ppi", "pubmed", "mit"]
+    dims = ["5", "10", "25", "50"]
+    methods = ["abrw", "attrpure", "deepwalk", "tadw", "aane", "sagegcn"]
+    seeds = [str(x) for x in range(30)]
+    exps = ["nc_experiment", "lp_experiment"]
+
+    for dataset, dim, method, seed, exp in itertools.product(
+        datasets, dims, methods, seeds, exps
+    ):
+        embedding_directory = os.path.join(
+            "embeddings", dataset, exp, dim, method, seed
+        )
+
+        filename = os.path.join(embedding_directory, "embedding.csv.gz")
+
+        try:
+            df = pd.read_csv(filename)
+        except EmptyDataError:
+            print (filename, "is empty")
+        except IOError:
+            print (filename, "does not exist")
+
+
+
+
+if __name__ == "__main__":
+    main()
